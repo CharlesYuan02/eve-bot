1726b957b5535644fb247fe32a7cefeeafb42a31
Charles Yuan <70110720+Chubbyman2@users.noreply.github.com>
2022-02-21 20:22:26 -0500

    Migrated to nextcord.

--- cogs/media.py
+++ cogs/media.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 import urllib.parse
 import urllib.request
 import re
@@ -7,7 +7,7 @@ import re
 
 class Media(commands.Cog):
 
-    def __init__(self, client):  # References whatever is passed through the client from discord
+    def __init__(self, client):
         self.client = client
 
     @commands.command(aliases=["video"])
@@ -17,16 +17,6 @@ class Media(commands.Cog):
             "https://www.youtube.com/results?search_query=" + search_keyword)
         video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
         await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
-
-    '''
-    @commands.command(aliases=["photo", "picture", "show"])
-    async def image(self, ctx, *, search):
-        search = search.replace(" ", "_")
-        html = urllib.request.urlopen(
-            f"https://www.google.com/search?q={search}&source=lnms&tbm=isch")
-        data = {'content': 'images src', 'src': re.findall('src="([^"]+)"', a)}
-        await ctx.send(data['src'][0])
-    '''
 
 
 def setup(client):
