acd9f714df4418e2c1af1201d57e5a4e82eae3f7
Charles Yuan <70110720+Chubbyman2@users.noreply.github.com>
2022-03-11 22:44:45 -0500

    Updated embed

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -20,7 +20,7 @@ class Wikipedia(commands.Cog):
             )
             wiki_embed.set_thumbnail(url="https://media.discordapp.net/attachments/952037974420385793/952038039457267712/Eve_Code_Ultimate_2.png")
             wiki_embed.set_footer(text="Github: https://github.com/Chubbyman2/eve-bot")
-            wiki_embed.add_field(name="Here is what I found:", value=response)
+            wiki_embed.add_field(name="Here is what I found:", value="```" + response + "```")
             await ctx.send(embed=wiki_embed)
         except:
             await ctx.send(f"Sorry, I couldn't find anything for {keywords}.")
