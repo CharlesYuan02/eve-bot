e3c6442ab9528d82f8b70152bd4890c1cd762f40
Charles Yuan <70110720+Chubbyman2@users.noreply.github.com>
2022-03-11 22:42:29 -0500

    Added embeds

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -13,7 +13,15 @@ class Wikipedia(commands.Cog):
         try:
             # Use repr to convert command object to string
             response = wikipedia.summary(repr(keywords), sentences=2)
-            await ctx.send("Here is what I found: \n" + response)
+            wiki_embed = nextcord.Embed(
+                title = "Wikipedia Query",
+                description = f"Query: {repr(keywords)}",
+                colour = 0x0adbfc
+            )
+            wiki_embed.set_thumbnail(url="https://media.discordapp.net/attachments/952037974420385793/952038039457267712/Eve_Code_Ultimate_2.png")
+            wiki_embed.set_footer(text="Github: https://github.com/Chubbyman2/eve-bot")
+            wiki_embed.add_field(name="Here is what I found:", value=response)
+            await ctx.send(embed=wiki_embed)
         except:
             await ctx.send(f"Sorry, I couldn't find anything for {keywords}.")
 
