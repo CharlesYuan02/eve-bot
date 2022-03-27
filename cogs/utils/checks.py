33c03a3713b33068769f6a49f915e2a802630642
Gerlesh <cjplanelles@gmail.com>
2022-03-27 15:28:33 -0400

    Replace custom checks with builtin permission checks

--- cogs/utils/checks.py
+++
@@ -1,7 +0,0 @@
-import nextcord
-from nextcord.ext import commands
-
-def is_admin():
-    async def predicate(ctx):
-        return ctx.author.guild_permissions.administrator
-    return commands.check(predicate)
